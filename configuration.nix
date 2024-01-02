e1198aacdc3adecfa27c2b719ea3c846cdb120b0
Francisco Demartino <git@fd1.com.ar>
2024-01-01 22:32:47 -0300

    limit bootloader entries

--- configuration.nix
+++ configuration.nix
@@ -16,6 +16,7 @@ in
     nix.settings.auto-optimise-store = true;
 
     boot.loader.systemd-boot.enable = true;
+    boot.loader.systemd-boot.configurationLimit = 7;
     boot.loader.efi.canTouchEfiVariables = true;
 
     powerManagement.enable = true;    # "stock NixOS power management tool"
