e639954111d49deb6955a790ff23b9c40414e2a8
Francisco Demartino <git@fd1.com.ar>
2024-03-10 00:36:21 -0300

    faster fish prompt

--- fish/config.fish
+++ fish/config.fish
@@ -1,3 +1,9 @@
 if status is-interactive
     set -g fish_greeting
+
+    function fish_vcs_prompt
+        # sloooow!
+        #
+        #         $ fish --profile prompt.prof -ic 'fish_prompt; exit'; cat prompt.prof | sort -nk 2
+    end
 end
