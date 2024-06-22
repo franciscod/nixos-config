7dc6c9638b655bffdcd0bda9491df9fba2fe2235
Francisco Demartino <git@fd1.com.ar>
2024-06-22 20:21:56 -0300

    add tpope's surround and repeat

--- nvim/setup.sh
+++ nvim/setup.sh
@@ -1,10 +1,12 @@
 mkdir -p pack/clones/start
 cd pack/clones/start
 
+git clone https://tpope.io/vim/repeat.git
 git clone https://tpope.io/vim/sensible.git
 git clone https://tpope.io/vim/sleuth.git && vim -u NONE -c "helptags sleuth/doc" -c q
 git clone https://tpope.io/vim/fugitive.git && vim -u NONE -c "helptags fugitive/doc" -c q
 git clone https://tpope.io/vim/eunuch.git && vim -u NONE -c "helptags eunuch/doc" -c q
+git clone https://tpope.io/vim/surround.git && vim -u NONE -c "helptags surround/doc" -c q
 git clone https://github.com/junegunn/fzf
 git clone https://github.com/junegunn/fzf.vim
 git clone https://github.com/sainnhe/everforest
